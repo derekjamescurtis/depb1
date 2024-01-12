d7cda43cbe5bb2490ac98313c5c1ec7fecb252e2
Derek J. Curtis <dcurtis@drivecentric.com>
2024-01-12 09:54:31 -0500

    meh

---
+++ dir1/providers.tf
@@ -0,0 +1,19 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.21.0"
+    }
+
+    fivetran = {
+      source  = "fivetran/fivetran"
+      version = "1.1.5"
+    }
+
+    mssql = {
+      source  = "PGSSoft/mssql"
+      version = "0.6.0"
+    }
+  }
+}
